67da8fa1cdce53658c1fd2278454822ce81b9c5e
ajayphulwade2 <ajay.phulwade2@gmail.com>
2025-01-23 22:56:33 +0530

    advs

--- src/main/java/com/ajinternatinal/AjINational/AjINationalApplication.java
+++ src/main/java/com/ajinternatinal/AjINational/AjINationalApplication.java
@@ -2,10 +2,9 @@ package com.ajinternatinal.AjINational;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+//@EnableJpaRepositories
 public class AjINationalApplication {
 
 	public static void main(String[] args) {
